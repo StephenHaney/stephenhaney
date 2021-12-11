cec36011e59ac22af54cdef3777c7b89e6aa1f4b
Stephen Haney <stephen.haney@gmail.com>
2021-12-10 20:12:51 -0800

    Files output to their own file name

--- scripts/src/bake_html.rs
+++ scripts/src/bake_html.rs
@@ -13,5 +13,16 @@ pub fn bake_html_file(path: &std::path::PathBuf) {
   baked_html = bake_markdown::bake_markdown(&baked_html);
 
   // Write the baked file
-  fs::write("./foo.html", baked_html).expect("Unable to write file");
+  if let Some(path_as_str) = path.to_str() {
+    // Build the output path
+    let final_path_string = path_as_str.replacen("src", "dist", 1);
+    let final_path = std::path::PathBuf::from(&final_path_string);
+    if let Some(final_path_directories) = final_path.parent() {
+      // Create the directories we need if they don't exist yet:
+      fs::create_dir_all(&final_path_directories)
+        .expect("Could not create directories for the dist folder");
+      // Write the file:
+      fs::write(final_path, baked_html).expect("Unable to write file");
+    }
+  }
 }
