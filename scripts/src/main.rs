99b651783dd170b90feed01d01d2d9ffa6840bbe
Stephen Haney <stephen.haney@gmail.com>
2021-12-13 22:58:50 -0800

    Remove changed files from watch cache, comment clean up

--- scripts/src/main.rs
+++ scripts/src/main.rs
@@ -11,24 +11,10 @@ use walkdir::WalkDir;
 mod bake_html;
 mod copy_file;
 
-// Watch directory, run build script on individual files that change
-
-// build script:
-//   1. goes through html file and looks for "<import"
-//     splits attributes, pulls file from src
-//     load src file, string replace attributes into {{ text }} templates
-//     string replace baked import into original file
-//   2. look for "markdown" attribute on html nodes
-//     runs markdown crate on them
-//     string replace finished html back into html file
-//   3. writes new string to preview directory
-//   4. runs a local server with hot reload on the preview directory
-//   5. has flag for preview / true build to out directory
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // Keep track of HTML we've already compiled so we don't have to do it multiple times
+    // Keep track of HTML we've already compiled so we don't have to do one multiple times
     // if a file is used multiple times in a graph
     let baked_html_cache = &mut HashMap::<PathBuf, String>::new();
 
@@ -57,6 +43,7 @@ fn build_directory(html_cache: &mut HashMap<PathBuf, String>, src_directory: &Pa
             }
         }
 
+        // We'll decide how to parse files by their file extensions
         let entry_path = PathBuf::from(entry.path());
         let file_extension = &entry_path.extension().and_then(std::ffi::OsStr::to_str);
 
@@ -64,14 +51,14 @@ fn build_directory(html_cache: &mut HashMap<PathBuf, String>, src_directory: &Pa
             // Run HTML through special processing:
             Some("html") => {
                 let local_copy_entry_path = entry_path.to_owned();
-
+                // Do we have the baked HTML in cache?
                 if html_cache.contains_key(&entry_path) == false {
                     // First time we've seen this file, bake it:
                     let baked_html = bake_html::bake_html_file(html_cache, &entry_path);
                     html_cache.insert(entry_path, baked_html);
                 }
 
-                // Write the baked file
+                // Write the baked file to disk
                 if let Some(path_as_str) = local_copy_entry_path.to_str() {
                     // Build the output path
                     let final_path_string = path_as_str.replacen("src", "dist", 1);
@@ -112,7 +99,7 @@ fn watch(html_cache: &mut HashMap<PathBuf, String>, src_directory: &std::path::P
         match rx.recv() {
             Ok(event) => match &event {
                 DebouncedEvent::Write(path) => {
-                    // todo delete from html_cache
+                    html_cache.remove(path);
                     bake_html::bake_html_file(html_cache, path);
                 }
                 _ => {
